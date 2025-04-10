40a182a67547b1cca408f6e1c1d9482b75a9400b
Ayush varma <144325384+Ayushdev08@users.noreply.github.com>
2025-04-10 17:29:03 +0530

    Add files via upload

---
+++ data/index.ts
@@ -0,0 +1,202 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Experience", link: "#experience" },
+    { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+    {
+        id: 1,
+        title: "I prioritize client collaboration, fostering open communication ",
+        description: "",
+        className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+        imgClassName: "w-full h-full",
+        titleClassName: "justify-end",
+        img: "/b1.svg",
+        spareImg: "",
+    },
+    {
+        id: 2,
+        title: "I'm very flexible with time zone communications",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 3,
+        title: "My tech stack",
+        description: "I constantly try to improve",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-center",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 4,
+        title: "Tech enthusiast with a passion for development.",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "/grid.svg",
+        spareImg: "/b4.svg",
+    },
+
+    {
+        id: 5,
+        title: "Currently working on a new project",
+        description: "finding!!",
+        className: "md:col-span-3 md:row-span-2",
+        imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+        titleClassName: "justify-center md:justify-start lg:justify-center",
+        img: "/b5.svg",
+        spareImg: "/grid.svg",
+    },
+    {
+        id: 6,
+        title: "Do you want to start a project together?",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+        img: "",
+        spareImg: "",
+    },
+];
+
+export const projects = [
+    {
+        id: 1,
+        title: "Chattify - Real Time Chat Application",
+        des: "A web application that Allows Chatting In real Time",
+        img: "/chatify.png",
+        link: "https://chatify-zefl.onrender.com",
+    },
+    {
+        id: 2,
+        title: "Task Dashboard",
+        des: "A Task Manager Web App.",
+        img: "/main.png",
+        link: "https://github.com/Ayushdev08/Task-Dashboard",
+    },
+    {
+        id: 3,
+        title: "SchoolHive",
+        des: "A decentralized application that allows users to mint NFTs using the Avalanche blockchain.",
+        img: "/screenshot.png",
+        link: "https://github.com/Ayushdev08/SchoolHive/tree/main",
+    },
+    {
+        id: 4,
+        title: "Family-Travel-tracker",
+        des: "A Family Travel Trcking through Svg Map.",
+        img: "/main-page.png",
+        link: "https://github.com/Ayushdev08/Family-Travel-tracker",
+    },
+];
+
+// export const testimonials = [
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+// ];
+
+export const companies = [
+    {
+        id: 1,
+        name: "cloudinary",
+        img: "/cloud.svg",
+        nameImg: "/cloudName.svg",
+    },
+    {
+        id: 2,
+        name: "appwrite",
+        img: "/app.svg",
+        nameImg: "/appName.svg",
+    },
+    {
+        id: 3,
+        name: "HOSTINGER",
+        img: "/host.svg",
+        nameImg: "/hostName.svg",
+    },
+    {
+        id: 4,
+        name: "stream",
+        img: "/s.svg",
+        nameImg: "/streamName.svg",
+    },
+    {
+        id: 5,
+        name: "docker.",
+        img: "/dock.svg",
+        nameImg: "/dockerName.svg",
+    },
+];
+
+export const workExperience = [
+    {
+        id: 1,
+        title: "Web Developer",
+        desc: "Built a student portal with Excel-based data visualization and JWT-authenticated login system.",
+        className: "md:col-span-2",
+        thumbnail: "/exp1.svg",
+    },
+    {
+        id: 2,
+        title: "Freelancer",
+        desc: "Worked on Various Web development Projects.",
+        className: "md:col-span-2",
+        thumbnail: "/exp1.svg",
+    },
+];
+
+export const socialMedia = [
+    {
+        id: 1,
+        img: "/git.svg",
+        link: "https://github.com/Ayushdev08"
+    },
+    {
+        id: 2,
+        img: "/twit.svg",
+        link: "https://x.com/AyushVarma"
+    },
+    {
+        id: 3,
+        img: "/link.svg",
+        link: "https://www.linkedin.com/in/ayush-varma03/"
+    },
+];
